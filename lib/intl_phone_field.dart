1ce19eed9a78b23dc9840211c4555ecad2bcb0d6
Ernesto Alfonso Caballero <erns70b@github.com>
2026-04-30 18:07:29 +0200

    fix: only strip country code from initialValue when number exceeds maxLength
    Cuban numbers start with 53 (same as country code). The unconditional
    stripping corrupted local numbers like 53XXXXXX → XXXXXX on load.

--- lib/intl_phone_field.dart
+++ lib/intl_phone_field.dart
@@ -328,11 +328,14 @@ class _IntlPhoneFieldState extends State<IntlPhoneField> {
       _selectedCountry = _countryList.firstWhere((item) => item.code == (widget.initialCountryCode ?? 'US'),
           orElse: () => _countryList.first);
 
-      // remove country code from the initial number value
-      if (number.startsWith('+')) {
-        number = number.replaceFirst(RegExp("^\\+${_selectedCountry.fullCountryCode}"), "");
-      } else {
-        number = number.replaceFirst(RegExp("^${_selectedCountry.fullCountryCode}"), "");
+      // remove country code from the initial number value only if
+      // the number is longer than maxLength (implying country code is included)
+      if (number.length > _selectedCountry.maxLength) {
+        if (number.startsWith('+')) {
+          number = number.replaceFirst(RegExp("^\\+${_selectedCountry.fullCountryCode}"), "");
+        } else {
+          number = number.replaceFirst(RegExp("^${_selectedCountry.fullCountryCode}"), "");
+        }
       }
     }
 
